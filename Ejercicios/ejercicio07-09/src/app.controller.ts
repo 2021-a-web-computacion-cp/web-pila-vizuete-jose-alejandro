3f92a2a4c612b8393595bcd8afe90059031359fe
Jose Pila <jose.pila@epn.edu.ec>
2021-09-07 10:56:02 -0500

    Creacion de las cookies

--- Ejercicios/ejercicio07-09/src/app.controller.ts
+++ Ejercicios/ejercicio07-09/src/app.controller.ts
@@ -3,10 +3,32 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('setear-cookie-insegura')
+  setearCookieInsegura(
+    @Req() req, //Request que nos llega del usuario
+    @Response() res,
+  ){
+    res.cookie(
+      'galletaInsegura',
+      'Tengo hambre',
+    );
+    res.cookie(
+      'galletaSegura',
+      'Web :3',
+      {
+        secure: true,
+      },
+    );
+    res.send('ok');
+  }
+  @Get ('mostrar-cookies')
+  mostrarCookie(@Req() )
 }
